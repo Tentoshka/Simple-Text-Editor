8ca90bc7e4887554e9cc3c5dc988e642d2710692
Tentoshka <lnvdmitry@outlook.com>
2020-07-03 15:33:11 +0300

    Line Wrap

--- src/Editor.java
+++ src/Editor.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 public class Editor {
     private JPanel panelMain;
@@ -22,8 +23,12 @@ public class Editor {
     private boolean isOpen = false;
     private String link;
 
-    public Editor() {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
 
+    public Editor() {
+        editorArea.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        editorArea.setLineWrap(true);
         openButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,7 +45,13 @@ public class Editor {
             private void openFile(String link) {
                 try(FileReader reader = new FileReader(link))
                 {
-                    editorArea.setText(Files.lines(Paths.get(link)).reduce("", String::concat));
+                    BufferedReader r = new BufferedReader(reader);
+                    editorArea.setText("");
+                    String line = r.readLine();
+                    while (line != null) {
+                        editorArea.append(line + "\n");
+                        line = r.readLine();
+                    }
                     isOpen = true;
                 }
                 catch(IOException ex){
@@ -58,9 +69,9 @@ public class Editor {
                     String text = editorArea.getText();
                     writer.write(text);
                     writer.flush();
-                    isOpen = false;
                 }
                 catch(IOException ex){
+                    isOpen = false;
                     editorArea.setText(ex.getMessage());
                     saveButton.setEnabled(isOpen);
                     editorArea.setEditable(isOpen);
@@ -72,7 +83,7 @@ public class Editor {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Editor");
-        frame.setSize(800, 600);
+        frame.setSize(WIDTH, HEIGHT);
         frame.setContentPane(new Editor().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
